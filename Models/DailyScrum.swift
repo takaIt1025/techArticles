d03fb54d0d7ac81eb065a80e396027c939bdc9ad
takaIt1025 <tky.ar2013@gmail.com>
2022-08-27 22:30:32 +0900

    iOS APP DEV TUTORIAL モデルの追加

---
+++ Models/DailyScrum.swift
@@ -0,0 +1,24 @@
+//
+//  DailyScrum.swift
+//  techArticles-iOS (iOS)
+//
+//  Created by Takaya Ito on 2022/08/27.
+//
+
+import Foundation
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, theme: .yellow),
+        DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .orange),
+        DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
+    ]
+}
