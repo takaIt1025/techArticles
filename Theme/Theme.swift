889e85ce35ef51a9531ead5a54894a03bac418a5
takaIt1025 <tky.ar2013@gmail.com>
2022-08-27 23:13:14 +0900

    iOS APP DEV TUTORIAL 余分なスペースの削除

--- Theme/Theme.swift
+++ Theme/Theme.swift
@@ -28,7 +28,7 @@ enum Theme: String {
     var accentColor: Color {
         switch self {
         case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
-        case .indigo    , .magenta, .navy, .oxblood, .purple: return .white
+        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
         }
     }
     var mainColor: Color {
