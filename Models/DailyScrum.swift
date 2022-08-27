4a8905eb8ce3202fd52b8f5459b5e6117b95b221
takaIt1025 <tky.ar2013@gmail.com>
2022-08-28 02:09:20 +0900

    iOS APP DEV TUTORIAL DailyScrumのコンストラクタを記載

--- Models/DailyScrum.swift
+++ Models/DailyScrum.swift
@@ -8,10 +8,18 @@
 import Foundation
 
 struct DailyScrum {
+    let id: UUID
     var title: String
     var attendees: [String]
     var lengthInMinutes: Int
     var theme: Theme
+    
+    init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+        self.id = id
+        self.title = title
+        self.attendees = attendees
+        self.theme = theme
+    }
 }
 
 extension DailyScrum {
