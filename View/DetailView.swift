a08d4e101e2128cee0c74a517e71dd6b3f755a1f
takaIt1025 <tky.ar2013@gmail.com>
2022-08-28 16:31:56 +0900

    iOS APP DEV TUTORIAL Editボタン押下時にシートモーダルを表示させるように修正

--- View/DetailView.swift
+++ View/DetailView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct DetailView: View {
     let scrum: DailyScrum
+    
+    @State private var isPresentingEditView = false
+    
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
@@ -40,11 +43,38 @@ struct DetailView: View {
                 }
             }
         }
+        .navigationTitle(scrum.title)
+        .toolbar{
+            Button("Edit") {
+                isPresentingEditView = true
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationView {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Done") {
+                                isPresentingEditView = false
+                            }
+                        }
+                    }
+            }
+        }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(scrum: DailyScrum.sampleData[0])
+        Group {
+            DetailView(scrum: DailyScrum.sampleData[0])
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
     }
 }
